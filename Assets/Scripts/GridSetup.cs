4f8b8283dcda75f790dc57f41bbf379b4034115c
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2018-10-28 13:24:48 -0500

    Create Grid and Populate with Sample Images

---
+++ Assets/Scripts/GridSetup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSetup : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject sampleImage;  //Square image used for testing
+    private int numberOfGridUnits = 16; //Set Medium Sized Grid with 16 units
+
+    private void Start()
+    {
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        GameObject newObject; //Create Instance of Game Object
+
+        for(int i=0; i<numberOfGridUnits;i++)
+        {
+            newObject = (GameObject)Instantiate(sampleImage, transform); //Creates objects to fill grid
+            newObject.GetComponent<Image>().color = Random.ColorHSV();
+        }
+
+    }
+}
