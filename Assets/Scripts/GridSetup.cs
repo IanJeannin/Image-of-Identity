a3150c90af1d870710f73053b8e871f5f4909b0b
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2018-10-28 16:49:07 -0500

    Add Methods for row/Colum Movement Buttons
    Move Row Right/Left Method finished. Need to implement in all buttons. Rows are reversed, row 0 is bottom row.

--- Assets/Scripts/GridSetup.cs
+++ Assets/Scripts/GridSetup.cs
@@ -10,6 +10,7 @@ public class GridSetup : MonoBehaviour {
     [SerializeField]
     private GameObject blankImage; //Blank Square used for empty spaces
 
+    private int rowColumnSize = 4;
     private int numberOfGridUnits = 16; //Set Medium Sized Grid with 16 units
     private bool [,] gridArray=new bool [4,4];  //Create a 2D array for keeping track of grid units
 
@@ -28,7 +29,7 @@ public class GridSetup : MonoBehaviour {
 
         for(int i=0; i<numberOfGridUnits;i++)
         {
-            if ((i+1)%4 != 0&&numberOfObjects>0) //If square is not in last column and there are still unused chosen images, fill the grid unit with an image
+            if ((i+1)%rowColumnSize != 0&&numberOfObjects>0) //If square is not in last column and there are still unused chosen images, fill the grid unit with an image
             {
                 newObject = (GameObject)Instantiate(sampleImage, transform); //Creates objects to fill grid
                 newObject.GetComponent<Image>().color = Random.ColorHSV(); //Gives Square random color
@@ -40,7 +41,7 @@ public class GridSetup : MonoBehaviour {
             {
                 newObject = (GameObject)Instantiate(blankImage, transform); //Creates blank square
                 gridArray[gridArrayRow, gridArrayColumn] = false; //Sets gridArray index as false
-                if((i+1)%4==0) //Prevents gridArrayColumn and Row from changing until 'i' reaches last column of grid
+                if((i+1)%rowColumnSize==0) //Prevents gridArrayColumn and Row from changing until 'i' reaches last column of grid
                 {
                   gridArrayColumn = 0; //Sets gridArrayColumn back to 0
                     gridArrayRow++; //Increases the gridArray row by 1
@@ -54,14 +55,165 @@ public class GridSetup : MonoBehaviour {
     {
         string arrayString = "";
 
-        for (int b = 0; b < 4; b++)
+        for (int b = 0; b < rowColumnSize; b++)
         {
-            for (int c = 0; c < 4; c++)
+            for (int c = 0; c < rowColumnSize; c++)
             {
               arrayString += string.Format("{0}", gridArray[b, c]);
             }
             arrayString += System.Environment.NewLine;
         }
         Debug.Log(arrayString);
+    }
+
+
+    //=====================================================================================================
+    //Next Methods call for a change in row/column based on which button was clicked
+    //======================================================================================================
+
+    public void row0Left()
+    {
+        rowLeft(0);
+    }
+
+    public void row1Left()
+    {
+        rowLeft(1);
+    }
+
+    public void row2Left()
+    {
+        rowLeft(2);
+    }
+
+    public void row3Left()
+    {
+        rowLeft(3);
+    }
+
+    public void row0Right()
+    {
+        rowRight(0);
+    }
+
+    public void row1Right()
+    {
+        rowRight(1);
+    }
+
+    public void row2Right()
+    {
+        rowRight(2);
+    }
+
+    public void row3Right()
+    {
+        rowRight(3);
+    }
+
+    public void column0Up()
+    {
+        columnUp(0);
+    }
+
+    public void column1Up()
+    {
+        columnUp(1);
+    }
+
+    public void column2Up()
+    {
+        columnUp(2);
+    }
+
+    public void column3Up()
+    {
+        columnUp(3);
+    }
+
+    public void column0Down()
+    {
+        columnDown(0);
+    }
+
+    public void column1Down()
+    {
+        columnDown(1);
+    }
+
+    public void column2Down()
+    {
+        columnDown(2);
+    }
+
+    public void column3Down()
+    {
+        columnDown(3);
+    }
+
+    //==================================================================================
+    // Functions that move rows/columns left/right/up/down
+    //==================================================================================
+
+    private void rowRight(int row) //Moves all units in a row right, until there are no blank spaces on the rightmost column
+    {
+        bool allEmpty = true; //If all grid units in row are empty
+        for(int x=0;x<rowColumnSize-1;x++) //Iterate through all grid units in row
+        {
+            if(gridArray[row,x]==true) //If one of the units has an image
+            {
+                allEmpty = false; //All Empty is false
+            }
+        }
+
+        if (allEmpty == false) //As long as at least one unit in the row has an image...
+        {
+            while (gridArray[row, rowColumnSize - 1] == false) //While the rightmost column has no image
+            {
+                for (int i = rowColumnSize - 1; i > 1; i--) //For each unit in the row
+                {
+                    gridArray[row, i] = gridArray[row, i - 1]; //Move all units in the row right
+                }
+                gridArray[row, 0] = false; //Make the leftmost column have no image
+            }
+            CheckArray(); //Check to make sure it works
+        }
+
+    }
+
+    private void rowLeft(int row)
+    {
+        bool allEmpty = true; //If all grid units in row are empty
+        for (int x = 0; x < rowColumnSize - 1; x++) //Iterate through all grid units in row
+        {
+            if (gridArray[row, x] == true) //If one of the units has an image
+            {
+                allEmpty = false; //All Empty is false
+            }
+        }
+
+        if (allEmpty == false) //As long as at least one unit in the row has an image...
+        {
+            while (gridArray[row, 0] == false) //Moves all units in a row left, until there are no blank spaces on the leftmost column
+            {
+                for (int i = 0; i < rowColumnSize - 1; i++) //While the leftmost column has no image
+                {
+                    gridArray[row, i] = gridArray[row, i + 1]; //For each unit in the row
+                }
+                gridArray[row, rowColumnSize - 1] = false; //Move all units in the row left
+            }
+            CheckArray(); //Check to make sure it works
+        }
+
+    }
+
+    private void columnUp(int column)
+    {
+
+    }
+
+    private void columnDown(int column)
+    {
+
     }
 }
