d2eb088937456cde087a58ff6d94cb4a9a7e46fe
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2018-10-28 13:44:30 -0500

    Add Blank spaces in grid
    If in last column or if all chosen images are used, fill grid unit with a blank image

--- Assets/Scripts/GridSetup.cs
+++ Assets/Scripts/GridSetup.cs
@@ -7,6 +7,9 @@ public class GridSetup : MonoBehaviour {
 
     [SerializeField]
     private GameObject sampleImage;  //Square image used for testing
+    [SerializeField]
+    private GameObject blankImage; //Blank Square used for empty spaces
+
     private int numberOfGridUnits = 16; //Set Medium Sized Grid with 16 units
 
     private void Start()
@@ -17,11 +20,20 @@ public class GridSetup : MonoBehaviour {
     private void CreateGrid()
     {
         GameObject newObject; //Create Instance of Game Object
+       int numberOfObjects = 9; //Sets number of Objects player has chosen
 
         for(int i=0; i<numberOfGridUnits;i++)
         {
-            newObject = (GameObject)Instantiate(sampleImage, transform); //Creates objects to fill grid
-            newObject.GetComponent<Image>().color = Random.ColorHSV();
+            if ((i+1)%4 != 0&&numberOfObjects>0) //If square is not in last column and there are still unused chosen images, fill the grid unit with an image
+            {
+                newObject = (GameObject)Instantiate(sampleImage, transform); //Creates objects to fill grid
+                newObject.GetComponent<Image>().color = Random.ColorHSV(); //Gives Square random color
+                numberOfObjects--;
+            }
+            else //If Square is in last column or all chosen images are used, fill with a blank space
+            {
+                newObject = (GameObject)Instantiate(blankImage, transform); //Creates blank square
+            }
         }
 
     }
